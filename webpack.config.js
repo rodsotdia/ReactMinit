75c270c015cdbb5187a42d42147cc9d6cd2884df
Rod Sot <hi@rodsot.com>
2022-01-12 23:01:34 +1300

    Install Webpack and loaders + Add webpack config

---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const path = require("path"); // Set up the path of the project
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+   entry: "./src/index.js",
+   output: {
+      path: path.resolve(__dirname, "dist"),
+      filename: "bundle.js",
+   },
+   resolve: {
+      extensions: [".js", ".jsx"],
+   },
+   module: {
+      rules: [
+         {
+            test: /\.(js|jsx)$/,
+            exclude: /node_modules/,
+            use: {
+               loader: "babel-loader",
+            },
+         },
+         {
+            test: /\.html$/,
+            use: [
+               {
+                  loader: "html-loader",
+               },
+            ],
+         },
+      ],
+   },
+   plugins: [
+      new HtmlWebpackPlugin({
+         template: "./public/index.html",
+         filename: "./index.html",
+      }),
+   ],
+};
