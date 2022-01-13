7df020d1d46a0f030aa3de0e062af4b20fee527f
Rod Sot <hi@rodsot.com>
2022-01-13 15:14:37 +1300

    Add routes structure and default pages

---
+++ src/pages/About.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const About = () => {
+   return (
+      <div>
+         <h1>This is the About</h1>
+         <Link to="/">Home</Link>
+      </div>
+   );
+};
+
+export default About;
--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -1,11 +1,17 @@
 import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "@pages/Home";
+import About from "@pages/About";
 import "@styles/global.scss";
 
 const App = () => {
    return (
-      <div>
-         <h1>Hola</h1>
-      </div>
+      <BrowserRouter>
+         <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="about" element={<About />} />
+         </Routes>
+      </BrowserRouter>
    );
 };
 
